648ee1e37f648e9bb9ded8e6d70f1a2a75662a5f
daeho.yang <sheepvs5@gmail.com>
2023-08-18 09:54:49 +0900

    Update VSI.py

--- IQA_pytorch/VSI.py
+++ IQA_pytorch/VSI.py
@@ -6,14 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from numpy.fft import ifftshift
 import math
-from .utils import abs, real, imag, spatial_normalize, downsample, prepare_image
-
-if hasattr(torch, 'rfft'):
-    rfft_func = torch.rfft
-elif hasattr(torch.fft, 'rfft'):
-    rfft_func = torch.fft.rfft
-else:
-    raise ModuleNotFoundError("rfft function is not found")
+from .utils import abs, real, imag, spatial_normalize, downsample, prepare_image, rfft
 
 eps = 1e-12
 
@@ -79,9 +72,9 @@ def SDSP(img,sigmaF,omega0,sigmaD,sigmaC):
 
     lab = rgb_to_lab_NCHW(img/255)
     LChannel, AChannel, BChannel = lab[:,0,:,:].unsqueeze(1),lab[:,1,:,:].unsqueeze(1),lab[:,2,:,:].unsqueeze(1)
-    LFFT = rfft_func(LChannel,2,onesided=False)
-    AFFT = rfft_func(AChannel,2,onesided=False)
-    BFFT = rfft_func(BChannel,2,onesided=False)
+    LFFT = rfft(LChannel,2,onesided=False)
+    AFFT = rfft(AChannel,2,onesided=False)
+    BFFT = rfft(BChannel,2,onesided=False)
     
     LG = logGabor(rows,cols,omega0,sigmaF)
     LG = torch.from_numpy(LG).reshape(1, 1, rows,cols,1).repeat(B,1,1,1,2).float().to(img.device)
